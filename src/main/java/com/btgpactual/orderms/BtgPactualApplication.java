80822e1d02208828ed8dc14e08197cc61b5da6f3
Vinicius de Moraes Espirito Santos de Oliveira <06644513140@seguranca.local>
2024-11-08 09:00:17 -0400

    :tada: feat: initial commit

---
+++ src/main/java/com/btgpactual/orderms/BtgPactualApplication.java
@@ -0,0 +1,13 @@
+package com.btgpactual.orderms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtgPactualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtgPactualApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/btgpactual/orderms/BtgPactualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btgpactual.orderms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtgPactualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
