c7d1c2c82cb07e0994a9930ebcae48af1f4bf2ce
Vinicius de Moraes Espirito Santos de Oliveira <06644513140@seguranca.local>
2024-11-08 09:13:27 -0400

    raw: configured rabbitmq queue

---
+++ src/main/java/com/btgpactual/orderms/config/RabbitMqConfig.java
@@ -0,0 +1,10 @@
+package com.btgpactual.orderms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+
+}
